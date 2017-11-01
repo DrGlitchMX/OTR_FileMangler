006c24e643559c4389c7824723f901abc9244318
Max Seifert <max.seifert@drglitch.net>
2017-11-01 23:47:09 +0100

    cleanup and rationalization
    rework code to meet better standards, mostly regarding naming.

--- otr/settings.py
+++ otr/settings.py
@@ -1,59 +1,40 @@
 # coding=utf-8
-'''
-Created on 08.01.2014
+"""
+Created on 2014-01-08
 
 @author: defiler
-'''
+"""
 
 
 # Do not process files if they are opened by a process belonging to these users.
 FILE_USERS = ['proftpd',]
 
-# directory which is scanned for incoming data
+# directory which is scanned for incoming movies and series
 SOURCE_PATH = '/opt/ftp'
 # base path of movie and series collection
 TARGET_BASE_PATH = '/mnt/nasgul.castle/Videos'
 # file for keeping track of already processed files
 PROCESSED_FILES = '/opt/ftp/otr_processed_files'
 
+MOVIES_SUBDIR = 'filme'
+SERIES_SUBDIR = 'series'
 
-CATEGORIES = ['filme', 'series', 'doku', 'misc',]
-SUB_CATEGORIES = {
-        'filme' : None,
-        'series' : [('American Dad',),
-                    ('Angel',),
-                    ('BigBangTheory',),
-                    ('Bones',),
-                    ('Buffy',),
-                    ('Charmed',),
-                    ('CSI',),
-                    ('Criminal Intent',),
-                    ('Dead Like Me',),
-                    ('Der Tatortreiniger',),
-                    ('Die Anstalt',),
-                    ('Dr. House',),
-                    ('Family Guy',),
-                    ('Farscape',),
-                    ('Firefly Season',),
-                    ('Futurama',),
-                    ('Grey s Anatomy',),
-                    ('Harvey Birdman',),
-                    ('heroes',),
-                    ('Homeland',),
-                    ('House MD',),
-                    ('IjonTichy',),
-                    ('Irgendwie und Sowieso',),
-                    ('Law and Order', 'Law und Order', 'Law Order',),
-                    ('Lilyhammer',),
-                    ('Mord mit Aussicht',),
-                    ('Navy CIS', 'NCIS',),
-                    ('Orphan Black',),
-                    ('Revenge',),
-                    ('Robot Chicken',),
-                    ('Sex and the City',),
-                    ('The Mentalist',),],
-        'doku' : None,
-        'misc' : None,
-        }
-
+# Currently used for series, only. Specify what subdirectories
+# of SERIES_SUBDIR you want covered.
+# If series naming occurrs in a unreliable way, specify alternative
+# names as additional entries in the tuple. The first entry will always
+# be regarded as the actual folder name.
+CATEGORIES = {
+    SERIES_SUBDIR : [
+        ('American Dad',),
+        ('Buffy',),
+        ('CSI',),
+        ('Family Guy',),
+        ('Farscape',),
+        ('Futurama',),
+        ('House MD',),
+        ('Law and Order', 'Law und Order', 'Law Order',),
+        ('Navy CIS', 'NCIS',),
+    ]
+}
 
